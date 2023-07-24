dbe5a508f15d7cf0ebcfeb7c168420b3f6d36da0
Artem <Elandriel@mail.ru>
2023-07-24 20:42:13 +0500

    changing the name of variables

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите число");
-string a = Console.ReadLine();
-int number = int.Parse(a);
-int sum = number * number;
-Console.WriteLine(sum);
+﻿Console.WriteLine("Введите wtkj число");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+int result = number * number;
+Console.WriteLine(result);
 
