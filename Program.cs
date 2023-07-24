8df68b1acd6ccb55b8704e6883e642a219fe3b32
Artem <Elandriel@mail.ru>
2023-07-24 21:32:53 +0500

    two withdrawal options

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите wtkj число");
-string userInput = Console.ReadLine() ?? "";
-int number = int.Parse(userInput);
+﻿Console.WriteLine("Введите  число");
+int number = int.Parse(Console.ReadLine() ?? "");
 int result = number * number;
-Console.WriteLine(result);
+Console.WriteLine("Результат возведения числа "+ number + " в квадрат это - " + result);//Конкатенация
+Console.WriteLine($"Результат возведения числа {number} в квадрат это - {result}"); //Интерполяция
 
